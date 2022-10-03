dabd32e6e9088e077cb8693a4bb376ba6fbcf85d
Tyler Tan <tylerjhtan888@gmail.com>
2022-10-03 20:53:21 +0800

    1.1
    Added suffix array implementation (cuz i forgot XD)

--- Run.java
+++ Run.java
@@ -17,28 +17,43 @@ public class Run {
      */
     public static void selectionSort(String input) {
 
-        // Get the several substrings within the main inputted String.
+        // Get the several suffixes within the main inputted String.
         int nLength = input.length();
-        String[] substrings = new String[nLength];
+        String[] suffixes = new String[nLength];
+
+        // While also generating an initial suffix array to be altered.
+        int[] suffixArray = new int[nLength];
+
         for (int i = 0; i < nLength; i++) {
-            substrings[i] = input.substring(i, nLength);
+            suffixes[i] = input.substring(i, nLength);
+            suffixArray[i] = i;
         }
 
-        // Sort the substrings using insertion sort.
+        // Sort the suffixes using insertion sort.
         for (int i = 0; i < nLength - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j < nLength; j++) {
-                if (substrings[minIndex].compareTo(substrings[j]) > 0)
+                if (suffixes[minIndex].compareTo(suffixes[j]) > 0)
                     minIndex = j;
             }
-            String temp = substrings[i];
-            substrings[i] = substrings[minIndex];
-            substrings[minIndex] = temp;
+            String temp1 = suffixes[i];
+            int temp2 = suffixArray[i];
+            suffixes[i] = suffixes[minIndex];
+            suffixArray[i] = suffixArray[minIndex];
+            suffixes[minIndex] = temp1;
+            suffixArray[minIndex] = temp2;
         }
 
-        // Printing the substrings to verify the corrected order.
+        // Printing the suffixes to verify the corrected order.
         for (int i = 0; i < nLength; i++) {
-            System.out.println(substrings[i]);
+            System.out.println(suffixes[i]);
+        }
+
+        System.out.println();
+        
+        // Printing the suffix array to verify the corrected order.
+        for (int i = 0; i < nLength; i++) {
+            System.out.print(suffixArray[i] + " ");
         }
     }
 
@@ -46,38 +61,43 @@ public class Run {
      * This method merges two sorted subarrays of strings—as indicated by
      * the parameters nStart, nMid, and nStart—into sorted order.
      * 
-     * @param substrings the list of suffixes to be sorted
-     * @param nStart     the starting index within substrings of the first half of
-     *                   suffixes to be sorted
-     * @param nMid       the ending index within substrings of the first half of
-     *                   suffixes to be sorted
-     * @param nEnd       the ending index within substrings of the second half of
-     *                   suffixes to be sorted
+     * @param suffixes    the list of suffixes to be sorted
+     * @param suffixArray the suffix array to be altered along with suffixes[]
+     * @param nStart      the starting index within suffixes of the first half of
+     *                    suffixes to be sorted
+     * @param nMid        the ending index within suffixes of the first half of
+     *                    suffixes to be sorted
+     * @param nEnd        the ending index within suffixes of the second half of
+     *                    suffixes to be sorted
      */
-    public static void merge(String substrings[], int nStart, int nMid, int nEnd) {
+    public static void merge(String suffixes[], int suffixArray[], int nStart, int nMid, int nEnd) {
 
         // Initialize needed varibables.
-        String[] temp = new String[nEnd - nStart + 1];
+        String[] temp1 = new String[nEnd - nStart + 1];
+        int[] temp2 = new int[nEnd - nStart + 1];
         int index1 = nStart, index2 = nMid + 1;
 
         // Execute merging algorithm.
         for (int i = 0; i < nEnd - nStart + 1; i++) {
 
             // Inserting from first half...
-            if (index2 == nEnd + 1 || (index1 < nMid + 1 && substrings[index1].compareTo(substrings[index2]) < 0)) {
-                temp[i] = substrings[index1];
+            if (index2 == nEnd + 1 || (index1 < nMid + 1 && suffixes[index1].compareTo(suffixes[index2]) < 0)) {
+                temp1[i] = suffixes[index1];
+                temp2[i] = suffixArray[index1];
                 index1++;
             }
             // Inserting from second half...
             else {
-                temp[i] = substrings[index2];
+                temp1[i] = suffixes[index2];
+                temp2[i] = suffixArray[index2];
                 index2++;
             }
         }
 
-        // Save changes to substrings[].
+        // Save changes to suffixes[].
         for (int i = nStart, j = 0; i < nEnd + 1; i++, j++) {
-            substrings[i] = temp[j];
+            suffixes[i] = temp1[j];
+            suffixArray[i] = temp2[j];
         }
     }
 
@@ -90,26 +110,38 @@ public class Run {
      */
     public static void mergeSort(String input) {
 
-        // Get the several substrings within the main inputted String.
+        // Get the several suffixes within the main inputted String.
         int nLength = input.length();
-        String[] substrings = new String[nLength];
+        String[] suffixes = new String[nLength];
+
+        // While also generating an initial suffix array to be altered.
+        int[] suffixArray = new int[nLength];
+
         for (int i = 0; i < nLength; i++) {
-            substrings[i] = input.substring(i, nLength);
+            suffixes[i] = input.substring(i, nLength);
+            suffixArray[i] = i;
         }
 
         // Divide the suffixes.
         int nMid = nLength / 2;
 
         // Conquer the suffixes
-        mergeSort(substrings, 0, nMid);
-        mergeSort(substrings, nMid + 1, nLength - 1);
+        mergeSort(suffixes, suffixArray, 0, nMid);
+        mergeSort(suffixes, suffixArray, nMid + 1, nLength - 1);
 
         // Combine the suffixes.
-        merge(substrings, 0, nMid, nLength - 1);
+        merge(suffixes, suffixArray, 0, nMid, nLength - 1);
 
-        // Printing the substrings to verify the corrected order.
+        // Printing the suffixes to verify the corrected order.
         for (int i = 0; i < nLength; i++) {
-            System.out.println(substrings[i]);
+            System.out.println(suffixes[i]);
+        }
+
+        System.out.println();
+
+        // Printing the suffix array to verify the corrected order.
+        for (int i = 0; i < nLength; i++) {
+            System.out.print(suffixArray[i] + " ");
         }
     }
 
@@ -117,28 +149,32 @@ public class Run {
      * This method is an auxiliary method to be used by the
      * main mergeSort() method, as seen above.
      * 
-     * @param substrings the list of suffixes to be sorted
-     * @param nStart     the starting index within substrings of the suffixes to be
-     *                   sorted
-     * @param nEnd       the ending index within substrings of the suffixes to be
-     *                   sorted
+     * @param suffixes    the list of suffixes to be sorted
+     * @param suffixArray the suffix array to be altered along with suffixes[]
+     * @param nStart      the starting index within suffixes of the suffixes to be
+     *                    sorted
+     * @param nEnd        the ending index within suffixes of the suffixes to be
+     *                    sorted
      */
-    public static void mergeSort(String substrings[], int nStart, int nEnd) {
+    public static void mergeSort(String suffixes[], int suffixArray[], int nStart, int nEnd) {
         if (nStart != nEnd) {
 
             // Divide the suffixes.
             int nMid = (nEnd + nStart) / 2;
 
             // Conquer the suffixes.
-            mergeSort(substrings, nStart, nMid);
-            mergeSort(substrings, nMid + 1, nEnd);
+            mergeSort(suffixes, suffixArray, nStart, nMid);
+            mergeSort(suffixes, suffixArray, nMid + 1, nEnd);
 
             // Combine the suffixes.
-            merge(substrings, nStart, nMid, nEnd);
+            merge(suffixes, suffixArray, nStart, nMid, nEnd);
         }
     }
 
     public static void main(String args[]) {
+        System.out.println("Selection Sort...");
+        selectionSort("tgtgtgtgcaccg");
+        System.out.println("\n\nMerge Sort...");
         mergeSort("tgtgtgtgcaccg");
     }
 }
